5398ca276fbd22899490bd4714f9a61a6abbfa23
Brian Drelling <brian.drelling@gmail.com>
2019-10-07 22:52:00 -0500

    Disabled Package.swift for the time being

--- Package.swift
+++ Package.swift
@@ -1,30 +1,30 @@
-// swift-tools-version:5.0
+// // swift-tools-version:5.0
 
-//  Copyright © 2019 SpotHero, Inc. All rights reserved.
+// //  Copyright © 2019 SpotHero, Inc. All rights reserved.
 
-import PackageDescription
+// import PackageDescription
 
-let package = Package(
-    name: "SpotHeroEmailValidator",
-    platforms: [
-        .iOS(.v8),          // minimum supported version via SPM
-        .macOS(.v10_10),    // minimum supported version via SPM
-        .tvOS(.v9),         // minimum supported version via SPM
-        // watchOS is unsupported
-    ],
-    products: [
-        .library(name: "SpotHeroEmailValidator", targets: ["SpotHeroEmailValidator"]),
-    ],
-    targets: [
-        .target(
-            name: "SpotHeroEmailValidator",
-            dependencies: []
-        ),
-        .testTarget(
-            name: "SpotHeroEmailValidatorTests",
-            dependencies: [
-                .target(name: "SpotHeroEmailValidator"),
-            ]
-        ),
-    ]
-)
+// let package = Package(
+//     name: "SpotHeroEmailValidator",
+//     platforms: [
+//         .iOS(.v8),          // minimum supported version via SPM
+//         .macOS(.v10_10),    // minimum supported version via SPM
+//         .tvOS(.v9),         // minimum supported version via SPM
+//         // watchOS is unsupported
+//     ],
+//     products: [
+//         .library(name: "SpotHeroEmailValidator", targets: ["SpotHeroEmailValidator"]),
+//     ],
+//     targets: [
+//         .target(
+//             name: "SpotHeroEmailValidator",
+//             dependencies: []
+//         ),
+//         .testTarget(
+//             name: "SpotHeroEmailValidatorTests",
+//             dependencies: [
+//                 .target(name: "SpotHeroEmailValidator"),
+//             ]
+//         ),
+//     ]
+// )
