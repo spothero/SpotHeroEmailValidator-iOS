de94b8c0a1829282d5f2854d0da19bf81816cdfd
Brian Drelling <brian.drelling@gmail.com>
2020-05-07 12:33:03 -0500

    Added bridging header, fixed demo app

---
+++ Config/SpotHeroEmailValidator-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  SpotHeroEmailValidator-Bridging-Header.h
+//  SpotHeroEmailValidator
+//
+//  Created by Brian Drelling on 4/30/20.
+//  Copyright © 2020 SpotHero, Inc. All rights reserved.
+//
+
+#ifndef SpotHeroEmailValidator_Bridging_Header_h
+#define SpotHeroEmailValidator_Bridging_Header_h
+
+#import "../Sources/SpotHeroEmailValidator/include/SHAutocorrectSuggestionView.h"
+#import "../Sources/SpotHeroEmailValidator/include/SHEmailValidationTextField.h"
+
+#endif /* SpotHeroEmailValidator_Bridging_Header_h */
