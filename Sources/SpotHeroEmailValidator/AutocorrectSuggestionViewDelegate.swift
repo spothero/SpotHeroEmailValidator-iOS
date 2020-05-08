1c3196fb42b8ebc3c0edec241d41487e1328ab1d
Brian Drelling <bdrelling@users.noreply.github.com>
2020-05-08 16:27:37 -0500

    IOS-2066: Converted SHAutocorrectSuggestionView into Swift (#30)

--- Sources/SpotHeroEmailValidator/AutocorrectSuggestionViewDelegate.swift
+++ Sources/SpotHeroEmailValidator/AutocorrectSuggestionViewDelegate.swift
@@ -1,14 +1,20 @@
+//  Copyright © 2019 SpotHero, Inc. All rights reserved.
 //
-//  AutocorrectSuggestionViewDelegate.swift
-//  SpotHeroEmailValidator
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
 //
-//  Created by Brian Drelling on 5/7/20.
-//  Copyright © 2020 SpotHero, Inc. All rights reserved.
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 import Foundation
 
-// Work In Progress -- This is a boilerplate file for Swift conversion. It is not included in the target.
 protocol AutocorrectSuggestionViewDelegate: AnyObject {
     func suggestionView(_ suggestionView: SHAutocorrectSuggestionView, wasDismissedWithAccepted accepted: Bool)
 }
