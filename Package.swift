c608aba5f159be4cae4a62356edf8095b4c05fa4
Brian Drelling <bdrelling@users.noreply.github.com>
2020-12-10 12:04:18 -0600

    IOS-2641: Migrated to SPM workspace (#36)

--- Package.swift
+++ Package.swift
@@ -1,33 +1,33 @@
-// Until Swift Package Manager has Resource Bundle support, Package.swift cannot be supported properly.
+// swift-tools-version:5.3
 
+//  Copyright © 2020 SpotHero, Inc. All rights reserved.
 
-// // swift-tools-version:5.0
+import PackageDescription
 
-// //  Copyright © 2019 SpotHero, Inc. All rights reserved.
-
-// import PackageDescription
-
-// let package = Package(
-//     name: "SpotHeroEmailValidator",
-//     platforms: [
-//         .iOS(.v8),          // minimum supported version via SPM
-//         .macOS(.v10_10),    // minimum supported version via SPM
-//         .tvOS(.v9),         // minimum supported version via SPM
-//         // watchOS is unsupported
-//     ],
-//     products: [
-//         .library(name: "SpotHeroEmailValidator", targets: ["SpotHeroEmailValidator"]),
-//     ],
-//     targets: [
-//         .target(
-//             name: "SpotHeroEmailValidator",
-//             dependencies: []
-//         ),
-//         .testTarget(
-//             name: "SpotHeroEmailValidatorTests",
-//             dependencies: [
-//                 .target(name: "SpotHeroEmailValidator"),
-//             ]
-//         ),
-//     ]
-// )
+let package = Package(
+    name: "SpotHeroEmailValidator",
+    platforms: [
+        .iOS(.v9),          // minimum supported version via SPM
+        .macOS(.v10_10),    // minimum supported version via SPM
+        .tvOS(.v9),         // minimum supported version via SPM
+        // watchOS is unsupported
+    ],
+    products: [
+        .library(name: "SpotHeroEmailValidator", targets: ["SpotHeroEmailValidator"]),
+    ],
+    targets: [
+        .target(
+            name: "SpotHeroEmailValidator",
+            dependencies: [],
+            resources: [
+                .copy("Data/DomainData.plist"),
+            ]
+        ),
+        .testTarget(
+            name: "SpotHeroEmailValidatorTests",
+            dependencies: [
+                .target(name: "SpotHeroEmailValidator"),
+            ]
+        ),
+    ]
+)
