31056795fde22f5805ddd4c9ba2f7fbe7448b368
Martin Jr Powlette <hi@martinjrpowlette.com>
2022-11-14 13:33:57 -0500

    swiftformat corrections

--- Tests/SpotHeroEmailValidatorTests/EmailComponentsTests.swift
+++ Tests/SpotHeroEmailValidatorTests/EmailComponentsTests.swift
@@ -22,25 +22,25 @@ class EmailComponentsTests: XCTestCase {
         }
     }
 
-    func testSyntaxValidator() {
+    func testEmailComponentsInit() {
         let tests = [
             // Successful Examples
             TestModel(email: "test@email.com",
                       expectedTo: .return(username: "test", hostname: "email", tld: "com")),
-
+            
             TestModel(email: "TEST@EMAIL.COM",
                       expectedTo: .return(username: "TEST", hostname: "email", tld: "COM")),
-
+            
             TestModel(email: "test+-.test@email.com",
                       expectedTo: .return(username: "test+-.test", hostname: "email", tld: "com")),
-
+            
             TestModel(email: #""JohnDoe"@email.com"#,
                       expectedTo: .return(username: #""JohnDoe""#, hostname: "email", tld: "com")),
-
+            
             // Failing Examples
             TestModel(email: "t@st@email.com", expectedTo: .throw(.invalidSyntax)),
             TestModel(email: "test.com", expectedTo: .throw(.invalidSyntax)),
-
+            
             // Domain Tests
             TestModel(email: "test@email", expectedTo: .throw(.invalidDomain)),
         ]
